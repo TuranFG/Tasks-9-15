23d44843dc7fe124b7f83ffb97f8cd7f8c6a3763
TuranFG <turanfg@code.edu.az>
2023-01-03 17:40:43 +0400

    Add project files.

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,140 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TASK15();
+        }
+
+        static void TASK9()
+        {
+            Console.WriteLine("Ededleri daxil edin");
+            double a = ReadInt32("a: ", 100000, 1000000);
+            double b = ReadInt32("b: ", 100000, 1000000);
+            double c = ReadInt32("c: ", 100000, 1000000);
+
+            a = a * 10 / 100;
+            b = b * 10 / 100;    
+            c = c * 10 / 100;
+
+            double cavab = (a+b+c)*10 / 100;
+            Console.WriteLine(cavab);
+        }
+
+        static void TASK10()
+        {
+            Console.WriteLine("Ededleri daxil edin");
+            double a = ReadInt32("a: ", 1000, 10000);
+            double b = ReadInt32("b: ", 1000, 10000);
+            double c = ReadInt32("c: ", 1000, 10000);
+
+            a = a * 1 / 100;
+            b = b * 2 / 100;
+            c = c * 3 / 100;
+
+            double cavab = a - b - c + (c*7/100);
+            Console.WriteLine(cavab);
+        }
+
+        static void TASK11()
+        {
+            Console.WriteLine("Ededleri daxil edin");
+            double a = ReadInt32("a: ", 10000, 100000);
+            double b = ReadInt32("b: ", 10000, 100000);
+            double c = ReadInt32("c: ", 10000, 100000);
+            double d = ReadInt32("d: ", 10000, 100000);
+               
+
+            double cavab = (a + c) * (b+d)-(c*3/100);
+            Console.WriteLine(cavab);
+        }
+
+        static void TASK12()
+        {
+            Console.WriteLine("Ededleri daxil edin");
+            double a = ReadInt32("a: ", 100000, 1000000);
+            double b = ReadInt32("b: ", 100000, 1000000);
+            double c = ReadInt32("c: ", 100000, 1000000);
+            double d = ReadInt32("d: ", 100000, 1000000);
+
+
+            double ededlerinOnFaizCemi = a*10/100+b*10/100+c*10/100+d*10/100;
+            double edelerinOnBeshFaizCemi= a * 15 / 100 + b * 15 / 100 + c * 15 / 100 + d * 15 / 100;
+            double cavab = (ededlerinOnFaizCemi * edelerinOnBeshFaizCemi * 10 / 100) * 11 / 100;
+            Console.WriteLine(cavab);
+        }
+
+        static void TASK13()
+        {
+            Console.WriteLine("Ededleri daxil edin");
+            double a = ReadInt32("a: ", 10000, 100000);
+            double b = ReadInt32("b: ", 10000, 100000);
+            double c = ReadInt32("c: ", 10000, 100000);
+            double d = ReadInt32("d: ", 100, 1000);
+            double e = ReadInt32("e: ", 100, 1000);
+
+
+            double beshReqemliEdedinBeshFaizHasili = (a * 5 / 100) * (b * 5 / 100) * (c * 5 / 100);
+            double uchReqemliEdedinUchFaizCemi = d * 3 / 100 + e * 3 / 100;
+            double cavab = beshReqemliEdedinBeshFaizHasili*10/100+uchReqemliEdedinUchFaizCemi*10/100;
+            Console.WriteLine(cavab);
+        }
+
+        static void TASK14()
+        {
+            Console.WriteLine("Ededleri daxil edin");
+            double a = ReadInt32("a: ", 100000, 1000000);
+            double b = ReadInt32("b: ", 100000, 1000000);
+            double c = ReadInt32("c: ", 100000, 1000000);
+            double d = ReadInt32("d: ", 100000, 1000000);
+            double e = ReadInt32("e: ", 100000, 1000000);
+            double f = ReadInt32("f: ", 100000, 1000000);
+
+            double Cem = a+b+c+d+e+f; 
+            double ac = a*1000000+c;
+            double cavab = ((Cem - ac) * 10 / 100 + e + f) * 11 / 100;
+            
+            Console.WriteLine(cavab);
+        }
+
+        static void TASK15()
+        {
+            Console.WriteLine("Ededleri daxil edin");
+            double a = ReadInt32("a: ", 100000, 1000000);
+            double b = ReadInt32("b: ", 10000, 100000);
+            double c = ReadInt32("c: ", 10000, 100000);
+            double d = ReadInt32("d: ", 1000, 10000);
+            double e = ReadInt32("e: ", 1000, 10000);
+            double f = ReadInt32("f: ", 100, 1000);
+            double g = ReadInt32("g: ", 100, 1000);
+
+
+
+            double cavab = (((((f + g + d * e) * 10 + 7 + b + c - (f * g * 10 + 1) + a - d + e + f + g) * 18 / 100) * 3 / 100) * 1 / 100) + b + c;
+
+            Console.WriteLine(cavab);
+        }
+
+
+
+        static int ReadInt32(string caption, int min, int max)
+        {
+            int a;
+        l1: Console.Write(caption, min, max);
+            if (int.TryParse(Console.ReadLine(), out a) == false)
+            {
+                Console.WriteLine("eded daxil edin");
+                goto l1;
+            }
+
+            if (a < max == false) { Console.WriteLine("duzgun araliqda ede daxil edin"); goto l1; }
+            if (a >= min == false) { Console.WriteLine("duzgun araliqda ede daxil edin"); goto l1; }
+
+            return a;
+
+        }
+    }
+}
---
+++ ConsoleApp16/Readme.md
@@ -0,0 +1,29 @@
+9) 3 dene 6 reqemli eded verilib. Her birinin 10 faizini tapib neticeleri topla. Alinan cavabin 10% tap.
+
+10) 3 dene 4 reqemli eded verilib.  I ededin 1%-ni, II ededin 2% , III ededin 3 % tap. 
+    Neticeleri bir birinden cix. Alinan cavabin ustune III ededin 7% faizini gel
+
+11) 4 dene 5 reqemli eded verilib. Her I ededin ustune III ededi gel . II ededin usutune IV eeddi gel. 
+    Sonra cavablari vur birbirine. Alinan neticeden III ededin 3%-ni cix.
+
+12) 4 dene 6 reqemli eded verilib. Ededlerin hamisinin 10 faizini tap ve topla. Sonra hamisinin 15 faizini tap ve topla. 
+    Sonra yekunda alinanlar iki cavabi vur biri birine. Alinan neticenin evvel 10% tap sonra ise hemin cavabin 11% tap.
+
+
+13) 5 dene eded verilib. Bunlardan 3 denesi 5 reqemli. 2 denesi ise 3 reqemlidir. 5 reqemli ededlerin 5% tap ,neticeleri vur bir birine. 
+    Sonra 3 reqemli ededlerin 3% tap ,neticeleri topla. Sonra yekunda alinan iki cavalarin her birinin 10%-ni tapib topla.
+
+
+14) 6 dene 6 reqemli eded verilib. Evvel hamisini topla . Sonra I ve III ededi bir birine yapishdir ve bir eded al.  
+    I neticeden II neticeni cix. Alinan cavabin 10% tap. Neticenin uzerine V ve VI ededleri gel. Yekunda alinan cavabin 11% tap.
+
+
+
+15)* 7 dene eded verilib.Bunlardan 2 denesi 3 reqemlidir. 2 denesi 4 reqemlidir. 2 denesi 5 reqemlidir.1 denesi ise 6 reqemlidir.
+    3 reqemli ededleri topla , sonra bunlarin ustune gel 4 reqemli ededlerin bir birine vurulmasindan alinan cavabi.
+    Sonra neticenin axirina 7 reqemini artir. Sonra cavabin ustune gel 5 reqemli ededlerin cemini.
+    Sonra neticeden cix 3 reqemli ededlerin bir birine vurulmasindan alinan cavabin axirina 1 artirilmish variantini.
+    Sonra alinan cavabin ustune gel 6 reqemli ededi.
+    Sonra alinan cavabdan cix 3 reqemli ve 4 reqemli ededlerin cemini.
+    Sonra alinan cavabin 18% -ni sonra 3%-ni sonra 1% tap.
+    Sonra alinan cavabin ustune 5 reqemli ededlerin cemini gel.
